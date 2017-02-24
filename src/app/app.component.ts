331e08ecac988b1efaccc4a4d844c54dd27b19a2
Andrew Hammer <backpackcoder@users.noreply.github.com>
2017-02-24 14:15:06 +0300

    Closed #3 - Create menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {MenuItem} from "./menuItem";
 
 @Component({
     selector: 'app-root',
@@ -6,12 +7,23 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app works!';
     email = 'info@itete.or.tz';
     phone = '+255 684 282 626';
     urls = {
         facebook: 'https://www.facebook.com/IteteHospital',
         twitter: 'https://twitter.com/itetetz',
         youtube: 'https://plus.google.com/u/0/b/102053817464261583240/102053817464261583240'
-    }
+    };
+
+    title = 'Itete Lutheran';
+    subtitle = 'Council Designated Hospital';
+    menuItems = [
+      new MenuItem('Home', true),
+        new MenuItem('About'),
+        new MenuItem('Departments'),
+        new MenuItem('Facilities'),
+        new MenuItem('Mobile Services'),
+        new MenuItem('Hospital Projects'),
+        new MenuItem('Contact Us')
+    ];
 }
