4bf149abb9b57667e044ae3a3f5a4e2d3e01c549
Andrew Hammer <backpackcoder@users.noreply.github.com>
2017-02-24 10:58:56 +0300

    Closed #7 - Add Karma HTML reporter

--- karma.conf.js
+++ karma.conf.js
@@ -2,42 +2,58 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
-    ],
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
-    preprocessors: {
-      './src/test.ts': ['angular-cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    remapIstanbulReporter: {
-      reports: {
-        html: 'coverage',
-        lcovonly: './coverage/coverage.lcov'
-      }
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'karma-remap-istanbul']
-              : ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', 'angular-cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-remap-istanbul'),
+            require('angular-cli/plugins/karma'),
+            require('karma-html-reporter')
+        ],
+        files: [
+            {pattern: './src/test.ts', watched: false}
+        ],
+        preprocessors: {
+            './src/test.ts': ['angular-cli']
+        },
+        mime: {
+            'text/x-typescript': ['ts', 'tsx']
+        },
+        remapIstanbulReporter: {
+            reports: {
+                html: 'coverage',
+                lcovonly: './coverage/coverage.lcov'
+            }
+        },
+        angularCli: {
+            codeCoverage: true,
+            config: './angular-cli.json',
+            environment: 'dev'
+        },
+        reporters: config.angularCli && config.angularCli.codeCoverage
+            ? ['progress', 'html', 'karma-remap-istanbul']
+            : ['progress', 'html'],
+        htmlReporter: {
+            outputDir: 'karma_html', // where to put the reports
+            templatePath: null, // set if you moved jasmine_template.html
+            focusOnFailures: true, // reports show failures on start
+            namedFiles: false, // name files instead of creating sub-directories
+            pageTitle: null, // page title for reports; browser info by default
+            urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+            reportName: 'report-summary-filename', // report summary filename; browser info by default
+
+            // experimental
+            preserveDescribeNesting: false, // folded suites stay folded
+            foldAll: false, // reports start folded (only with preserveDescribeNesting)
+        },
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
+    console.log(config.angularCli);
 };
